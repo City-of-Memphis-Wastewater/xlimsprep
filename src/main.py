39f9308e0d4d099bf4682b8dc7cc24dde5c03a7d
George Clayton Bennett <GEORGE.BENNETT@memphistn.gov>
2025-06-03 11:37:46 -0500

    initial commit to dev branch

--- src/main.py
+++ src/main.py
@@ -23,15 +23,15 @@ def main():
     # Import data from xlims export files (manually prepared)
     parameter_units_dictionary_aggregate = {}
 
-    for csv_file in get_csv_files_relative_to_main():
+    for csv_filename in get_csv_files_relative_to_main():
         
-        df_xlims_primary = import_csv_to_pd_df(csv_file = "primary.csv")
+        df_xlims_primary = import_csv_to_pd_df(csv_file = csv_filename)
         #df_xlims_other = import_csv_to_pd_df(filename = "other.csv") #spoof
 
         # Sanitize data, convert to unique columns 
         df_sanitized_primary, parameter_units_dictionary = convert_xlims_data_to_columns(df_xlims_primary)
         parameter_units_dictionary_aggregate = check_for_diversity_in_parameter_units_dictionary(parameter_units_dictionary_aggregate,parameter_units_dictionary)
-        plot_sanitized(df_sanitized=df_sanitized_primary, tag=convert_filename_to_title(csv_file))
+        plot_sanitized(df_sanitized=df_sanitized_primary, tag=convert_filename_to_title(csv_filename))
     
 if __name__ == "__main__":
     main()
