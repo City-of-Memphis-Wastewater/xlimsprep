c0feadd5421147c41d6f849b2eb4a40408d63203
George Clayton Bennett <GEORGE.BENNETT@memphistn.gov>
2025-06-04 01:25:36 -0500

    correct error for linux installation: source venv/bin/activate --> source .venv/bin/activate

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat main.sh
 python -m venv .venv 
-source venv/bin/activate # linux
+source .venv/bin/activate # linux
 pip install -r requirements.txt
 python -m src.main
 python -m src.view
